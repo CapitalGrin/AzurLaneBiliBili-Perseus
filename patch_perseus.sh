587710344e7829e0bd29457cae0fb6ad4ba842a9
CapitalGrin <109933411+CapitalGrin@users.noreply.github.com>
2023-06-29 15:23:43 +0800

    Update patch_perseus.sh

--- patch_perseus.sh
+++ patch_perseus.sh
@@ -4,12 +4,8 @@
 
 if [ ! -f "com.bilibili.AzurLane.apk" ]; then
     echo "Get Azur Lane apk"
-    wget https://pkg.biligame.com/games/blhx_7.1.1_20230610_1_20230616_031339_073b2.apk -O com.bilibili.AzurLane.xapk -q
+    wget https://pkg.biligame.com/games/blhx_7.1.1_20230610_1_20230616_031339_073b2.apk -O com.bilibili.AzurLane.apk -q
     echo "apk downloaded !"
-    unzip -q -o com.bilibili.AzurLane.xapk -d AzurLane 
-    ls
-    echo "unzipped !"
-    cp AzurLane/com.bilibili.AzurLane.apk .
 fi
 
 # Download Perseus
